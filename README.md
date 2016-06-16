7fd3f8383d5323a916bb4875c230a02f05692f49
orkhanfarmanli <orkhanfarmanli@gmail.com>
2016-06-16 17:15:19 +0400

    Created basic structure of the project

---
+++ README.md
@@ -0,0 +1,8 @@
+#Chpitter Project
+
+###instructions for students:
+
+*Create vendors folder
+*add Bootstrap (Sass) to vendors folder
+*add jQuery to vendors folder
+*associated sass partials are in assets/sass/partials directory
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Chpitter</title>
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
+</head>
+
+<body>
+    <div class="container-fluid">
+
+      <!-- header section { -->
+        <div class="row">
+            <section class="col-md-10 col-md-offset-1" id="header">
+              <div class="col-md-12"></div> <!-- Farid + Eldar -->
+            </section>
+        </div>
+      <!-- header section } -->
+
+      <!-- body section { -->
+        <div class="row">
+            <section class="col-md-10 col-md-offset-1" id="body">
+              <div id="profile-column" class="col-md-3"></div> <!-- Nurgul -->
+              <div id="body-column" class="col-md-6"></div> <!-- Iso -->
+              <div id="suggestion-column" class="col-md-3"></div> <!-- Mahtab -->
+            </section>
+        </div>
+      <!-- body section } -->
+
+    </div>
+</body>
+<script src="vendors/jQuery/jquery.js" charset="utf-8"></script>
+<script src="assets/scripts/app.js" charset="utf-8"></script>
+</html>
